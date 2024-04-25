22e4f85046dfa57102fbc375966cbaa4795d51cb
hdnguyendev <nguyenhd.dev@gmail.com>
2024-04-25 22:30:00 +0700

    api response use class

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,8 +1,16 @@
 "use strict";
 const AccessService = require("../services/access.service");
+
+const { OK, CREATED } = require("../core/success.response");
 class AccessController {
   signUp = async (req, res, next) => {
-    res.status(201).json(await AccessService.signUp(req.body));
+    new CREATED({
+      message: "Registered Successfully",
+      metadata: await AccessService.signUp(req.body),
+      // option: {
+      //   limit: 10
+      // }
+    }).send(res);
   };
 }
 
